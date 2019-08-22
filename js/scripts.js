f8b4ea2dbd70d2c8abf0f19c8bc52c42e2cf238a
David Boedigheimer <stealth259@gmail.com>
2019-08-22 13:57:03 -0700

    Add ready function to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
