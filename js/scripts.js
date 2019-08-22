a50834d8ce871fc65135939e319d1ba3ab25a54b
David Boedigheimer <stealth259@gmail.com>
2019-08-22 13:42:26 -0700

    Add to script.js file.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
